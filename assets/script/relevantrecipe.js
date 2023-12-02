3eb24dcfc777a4604e660e34c819b833838e23ec
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2023-12-01 23:05:17 -0400

    created js file for each state

---
+++ assets/script/relevantrecipe.js
@@ -0,0 +1,19 @@
+// Displaying state: present user with a list of
+// relevant recipes options
+
+
+
+
+// Displaying state: user selects a specific recipe or
+// ask for more details
+
+
+
+
+
+
+
+// Displaying state: transition to the "displaying state"
+// if user selected recipe option via --> relevantrecipe.js
+// otherwise user can mark recipe as not relevant (can be used
+//  to train model. User need to modify/change question
---
+++ assets/script/search.js
@@ -0,0 +1,20 @@
+// Recipe search state: determine the specific details of
+// the recipe requested such as dishes, ingredients, cuisine
+
+
+
+// Recipe search state: use a recipe API or database to
+//  search for relevant recipes based on the user's criteria
+
+
+
+
+
+
+
+
+
+
+// Recipe search state: transition to the "displaying state"
+// once the recipes are obtained via --> relevantrecipe.js
+// otherwise if not found show user error searching recipe
