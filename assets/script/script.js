d716f78d2d329206f85395590be007b6320d5f44
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2023-12-01 21:29:25 -0400

    edit html and script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,23 @@
-// 
-document.addEventListener('DOMContentLoaded', () => {
-    const startBtn = document.getElementById('startBtn');
-    const resultDiv = document.getElementById('result');
 
-    
+// Listening state when user record/stop/verify/send audio/text
+document.addEventListener('DOMContentLoaded', () => {
+    // declare button listeners
+    const recordBtn = document.getElementById('record-btn');
+    const stopBtn = document.getElementById('stop-btn');
+    const verifyBtn = document.getElementById('verify-btn');
+    const sendBtn = document.getElementById('send-btn');
+
 });
+
+// using web speech api - speech recognition and speech synthesis
+
+
+// Fuo verify audio recording
+
+// Send audio recording to process speech recognition with wit.ai
+
+
+
+
+
+
