ad047613d313f256eab4a6b52a2819d02981b542
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2023-12-02 18:18:49 -0400

    implemented recipe API search in search.js

--- assets/script/understand.js
+++ assets/script/understand.js
@@ -7,6 +7,13 @@ $("#send-btn").click(function() {
     useWit(userRequest);
 });
 
+// add global variable for user recipe intent
+// to use the result of userRequest on search.js
+let userIntent = ""; // intitialize it
+
+// Use Wit.ai which is a natural language processing (NLP) platform
+// - used for building applications that can understand and interpret human language
+//  Goal is to train wit.ai applications to understand the intent behind user input
 function useWit(userRequest) {
     console.log("started using Wit function")
 
@@ -14,8 +21,6 @@ function useWit(userRequest) {
     // the user's request in wit.ai
     // That is, turn the message into actionable search query
 }
-
-
 
 // Understanding state: transition to the "recipe search state"
 // if the translation intent is detected via --> search.js
