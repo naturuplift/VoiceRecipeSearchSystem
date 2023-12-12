8fdc5c42ceed844a336df2d2d9d3504ffe585442
Samantha Allen <samanthaamiraallen@gmail.com>
2023-12-12 14:04:05 -0400

    changed API keys

--- assets/script/search.js
+++ assets/script/search.js
@@ -31,7 +31,7 @@ function searchRecipeOptions(searchType,searchRecipe){
 function getRecipeList(userRecipeList){
 
     let query = userRecipeList;
-    let AUTH = 'apiKey=3a12b110705e48fab6dd9e0ae25f9a15';
+    let AUTH = 'apiKey=8ed9b997410c46de90674787041efc93';
     // let NUMBER = 'number=9'; // recipes search number
     // const uri = 'https://api.spoonacular.com/recipes/complexSearch?query=' + query + '&' + AUTH + '&' + NUMBER;
     const uri = 'https://api.spoonacular.com/recipes/complexSearch?query=' + query + '&' + AUTH;
